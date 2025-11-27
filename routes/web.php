2a6f39f5b55e9fce02b89b513a9c0964d89ddb78
M. Bintang Hidayatullah <bintstar44@gmail.com>
2025-11-27 21:28:38 +0700

    Fix routes and move fallback outside auth middleware

--- routes/web.php
+++ routes/web.php
@@ -99,8 +99,9 @@ Route::middleware('auth')->group(function () {
         Route::get('/api/histori-kuis/{historiId}', [KuisController::class, 'getDetailHistori'])->name('api.histori-kuis.detail');
         Route::delete('/api/histori-kuis/{historiId}', [KuisController::class, 'destroyHistori'])->name('api.histori-kuis.destroy');
     });
-    Route::fallback(function () {
-    return response()->view('errors.404', [], 404);
+
 });
 
+Route::fallback(function () {
+    return response()->view('errors.404', [], 404);
 });
